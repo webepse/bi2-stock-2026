a98d3e238b2b346510ee6e279054e7ef0fc4b5dc
Boderio <berti@epse.be>
2025-10-14 13:11:31 +0200

    gestion catégories

---
+++ admin/categories.php
@@ -0,0 +1,63 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['login']))
+    {
+        header("LOCATION:index.php");
+        exit();
+    }
+
+    // besoin de la bdd
+    require "../config/connexion.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Stock - Administration - Gestion des catégories</title>
+</head>
+<body>
+    <?php
+        include("partials/nav.php");
+    ?>
+    <div class="container-fluid">
+        <h1>Gestion des catégories</h1>
+        <a href="#" class="btn btn-primary my-2">Ajouter une catégorie</a>
+        <table class="table table-striped">
+
+            <thead>
+                <tr class="text-center">
+                    <th>#</th>
+                    <th>Nom</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                  
+                    $categories = $bdd->query("SELECT * FROM categories");
+                    while($donCat = $categories->fetch())
+                    {
+                        echo '<tr class="text-center">';
+                            echo '<td>'.$donCat['id'].'</td>';
+                            echo '<td>'.$donCat['name'].'</td>';
+                            echo '<td>';
+                                echo '<a href="#" class="btn btn-warning">Modifier</a>';
+                                echo '<a href="#" class="btn btn-danger mx-2">Supprimer</a>';
+                            echo '</td>';
+                        echo '</tr>';
+                    }
+                    // à cause du système de cursor et qu'on boucle plusieurs fois, on doit close le cursor
+                    $categories->closeCursor();
+                ?>
+            </tbody>
+
+        </table>
+
+    </div>
+</body>
+</html>
--- admin/partials/nav.php
+++ admin/partials/nav.php
@@ -13,7 +13,7 @@
           <a class="nav-link" href="products.php">Produits</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Catégories</a>
+          <a class="nav-link" href="categories.php">Catégories</a>
         </li>
       </ul>
       <ul class="navbar-nav ms-auto">
