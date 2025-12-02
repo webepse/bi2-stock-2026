9eb5f0aaa3d8ee893a4c6a5f67f80eb81beae1af
Boderio <berti@epse.be>
2025-12-02 11:33:50 +0100

    galerie d'image avec bootstrap

--- product.php
+++ product.php
@@ -36,17 +36,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap-5.3.8-dist/css/bootstrap.min.css">
+    <script src="assets/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="assets/style.css">
-    <title>Document</title>
+    <title>BI2 - Stock - <?= $don['name'] ?></title>
 </head>
 <body>
 <?php
     include("partials/nav.php");
 ?>
-    <?php echo $don['name'] ?>
-    <h1><?= $don['name'] ?></h1>
-    <img src="images/<?= $don['cover'] ?>" alt="image de <?= $don['name'] ?>">
-    <h4><?= $don['date'] ?></h4>
-    <div><?= $don['description'] ?></div>
+<div class="container">
+    <div class="row my-5">
+        <div class="col-md-6">
+            <img src="images/<?= $don['cover'] ?>" alt="image de <?= $don['name'] ?>" class="img-fluid">
+        </div>
+        <div class="col-md-6">
+            <h1><?= $don['name'] ?></h1>
+            <h4><?= $don['date'] ?></h4>
+            <div><?= $don['description'] ?></div>
+        </div>
+    </div>
+    <h4>Galerie d'image</h4>
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <?php
+                $galerie = $bdd->prepare("SELECT * FROM images WHERE id_product=?");
+                $galerie->execute([$id]);
+                $count = $galerie->rowCount();
+                var_dump($count);
+                $cpt = 1;
+                while($donGal = $galerie->fetch())
+                {
+                    if($cpt == 1)
+                    {
+                        echo "<div class='carousel-item active'>";
+                    }
+                    else{
+                        echo "<div class='carousel-item'>";
+                    }
+                            echo "<img src='images/".$donGal['fichier']."' class='d-block w-100' alt='image de galere de".$don['name']."'>";
+                        echo "</div>";
+                    $cpt++;
+                }
+                $galerie->closeCursor();
+
+            ?>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
 </body>
 </html>
