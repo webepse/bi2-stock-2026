2c849ce215cd46a867d43c42c6961b1754d6bca9
Boderio <berti@epse.be>
2025-12-09 09:16:51 +0100

    modif bdd pour contact

--- bdd/127_0_0_1.sql
+++ bdd/127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 02 déc. 2025 à 08:01
+-- Généré le : mar. 09 déc. 2025 à 08:14
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -71,13 +71,29 @@ INSERT INTO `categories` (`id`, `name`, `image`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `message` text COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `images`
 --
 
 DROP TABLE IF EXISTS `images`;
 CREATE TABLE IF NOT EXISTS `images` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `fichier` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `fichier` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `id_product` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_product` (`id_product`)
