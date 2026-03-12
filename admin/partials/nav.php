37b9947394e54f781b4cca0646ccfbf26a828461
berti <berti@CLASSES.LOCAL>
2026-03-12 10:25:38 +0100

    skills add

--- admin/partials/nav.php
+++ admin/partials/nav.php
@@ -16,6 +16,9 @@
           <a class="nav-link" href="categories.php">Catégories</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="skills.php">Compétences</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="contact.php">Contact</a>
         </li>
       </ul>
---
+++ admin/skills.php
@@ -0,0 +1,97 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['login']))
+    {
+        header("LOCATION:index.php");
+        exit();
+    }
+
+    // besoin de la bdd
+    require "../config/connexion.php";
+
+    if(isset($_GET['delete']) && is_numeric($_GET['delete']))
+    {
+        
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Stock - Administration - Gestion des compétences</title>
+</head>
+<body>
+    <?php
+        include("partials/nav.php");
+    ?>
+    <div class="container-fluid">
+        <h1>Gestion des compétences</h1>
+        <a href="addSkills.php" class="btn btn-primary my-2">Ajouter une compétence</a>
+        <?php
+            if(isset($_GET['add']) && $_GET['add']=="success")
+            {
+                echo "<div class='alert alert-success'>Vous avez bien ajouté une nouvelle compétence à la base de données</div>";
+            }
+            if(isset($_GET['update']) && is_numeric($_GET['update']))
+            {
+                 echo "<div class='alert alert-warning'>Vous avez bien modifié la compétence n°".$_GET['update']." à la base de données</div>";
+            }
+        ?>
+        <table class="table table-striped">
+
+            <thead>
+                <tr class="text-center">
+                    <th>#</th>
+                    <th>Nom</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                  
+                    $skills = $bdd->query("SELECT * FROM skills");
+                    while($donSkill = $skills->fetch())
+                    {
+                        echo '<tr class="text-center">';
+                            echo '<td>'.$donSkill['id'].'</td>';
+                            echo '<td>'.$donSkill['nom'].'</td>';
+                            echo '<td>';
+                                echo '<button type="button" class="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#deleteModal'.$donSkill['id'].'">
+  supprimer
+</button>';
+                            echo '<div class="modal fade" id="deleteModal'.$donSkill['id'].'" tabindex="-1" aria-labelledby="exampleModalLabel'.$donSkill['id'].'" aria-hidden="true">';
+                                echo '<div class="modal-dialog">';
+                                    echo '<div class="modal-content">';
+                                        echo ' <div class="modal-header">';
+                                        echo '<h1 class="modal-title fs-5" id="exampleModalLabel'.$donSkill['id'].'">Supprimer #'.$donSkill['id'].'</h1>';
+                                        echo '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
+                                    echo ' </div>';
+                                        echo ' <div class="modal-body">';
+                                            echo 'Voulez-vous vraiment supprimer la compétence: "'.$donSkill['nom'].'" ?';;
+                                        echo '</div>';
+                                        echo ' <div class="modal-footer">';
+                                            echo ' <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Ne pas supprimer</button>';
+                                            echo '<a href="skills.php?delete='.$donSkill['id'].'" class="btn btn-danger mx-2">Supprimer</a>';
+                                        echo '</div>';
+                                    echo '</div>';
+                                echo '</div>';
+                            echo '</div>';
+
+                            echo '</td>';
+                        echo '</tr>';
+                    }
+                    // à cause du système de cursor et qu'on boucle plusieurs fois, on doit close le cursor
+                    $skills->closeCursor();
+                ?>
+            </tbody>
+
+        </table>
+
+    </div>
+</body>
+</html>
